5bb556b57514aff221ac2bbe94e60129c989ba41
Bryan Hunt <bryan.hunt@everbank.com>
2016-06-10 17:29:10 -0400

    adding authentication

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,12 @@
 class CommentsController < ApplicationController
-  def create 
+  before_filter :require_login, except [:create]
+
+  def create
     @comment = Comment.new(comment_params)
     @comment.article_id = params[:article_id]
 
     @comment.save
-    
+
     redirect_to article_path(@comment.article)
   end
 
